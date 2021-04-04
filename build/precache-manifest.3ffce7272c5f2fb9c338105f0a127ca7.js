d75806ed0f0bce76215d5677ffb03296615d43e5
tradelearn13 <tradelearn13@gmail.com>
2021-04-04 12:28:36 +0100

    commit

---
+++ build/precache-manifest.3ffce7272c5f2fb9c338105f0a127ca7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c0b94bb6d8ce8609cb7d319aeef94a",
+    "url": "/EricOgbe.github.io/contacts-app/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "/EricOgbe.github.io/contacts-app/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "fa99d2350ff4a8174190",
+    "url": "/EricOgbe.github.io/contacts-app/static/js/main.a3604650.chunk.js"
+  },
+  {
+    "revision": "cfae2adc1c5f24fa07e6",
+    "url": "/EricOgbe.github.io/contacts-app/static/js/runtime-main.c92e8485.js"
+  }
+]);
